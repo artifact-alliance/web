c73ecd1131b126466f9b229c2b36b86ea98eaaf5
Ethan Li <fourdimentional31@gmail.com>
2026-04-26 11:39:23 -0400

    build v10

--- main.dart.js
+++ main.dart.js
@@ -7869,7 +7869,7 @@ i.aD()
 i.a5R(h,h,r)
 s.ch$!==$&&A.aj()
 s.ch$=i
-j=i}s.a0n(new A.D7(p,new A.Mm(new A.a_u("http://http://192.168.0.223:5888"),h),o,j,h))
+j=i}s.a0n(new A.D7(p,new A.Mm(new A.a_u("192.168.0.223:5888"),h),o,j,h))
 s.M3()},
 Mm:function Mm(a,b){this.c=a
 this.a=b},
